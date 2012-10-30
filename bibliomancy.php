7896433703081aeac00cdd57dab055ca28efd8a0
Mita Williams <mita.williams@gmail.com>
2012-10-29 22:25:40 -0400

    random call number selection is now working

--- bibliomancy.php
+++ bibliomancy.php
@@ -1,16 +1,23 @@
 <?php
 
-	require ("db-vars.php");
-	$dbc = mysql_connect($host,$username,$password) or die ('will not connect');
-	mysql_select_db(DATABASE_NAME) or die (mysql_error());
+	require ("../db-vars.php");
+	$dbc = mysql_connect($host, $username, $password) or die (mysql_error());
+	mysql_select_db($database_name) or die (mysql_error());
 
+	$randomLCclass = rand(1,7);
+	$query = "SELECT * FROM " . TABLE_NAME . " WHERE LC_id=$randomLCclass";
 
-	$query = "SELECT * FROM " . TABLE_NAME;
+	$result = mysql_query($query, $dbc) or die ('will not query');
 
-	echo $query . '</br>';
-	echo $dbc . '</br>';
+	print 'A randomly generated call number. Hit refresh and see!'  . '</br>';
 
-	$result = mysql_query($query,$dbc) or die ('will not query');
+	$row = mysql_fetch_row($result);
+	$randomLCrange = rand(1,$row[2]);
+	echo $row[1] . $randomLCrange . '<br/>';
+
+	// Much thanks to everyone who helped!
+	// This is still work to be done. I need to add the cutter selector 
+	// and add the rest of the call numbers to the mysql table
 
 	mysql_close($dbc);
 
