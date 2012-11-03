486f6303b597a0fa744cd91f47e586f6a01c443c
Mita Williams <mita.williams@gmail.com>
2012-11-03 08:24:44 -0400

    finished verison : a simple call number picker

--- bibliomancy.php
+++ bibliomancy.php
@@ -1,24 +1,28 @@
 <?php
 
-	require ("../db-vars.php");
-	$dbc = mysql_connect($host, $username, $password) or die (mysql_error());
-	mysql_select_db($database_name) or die (mysql_error());
+  require ("../db-vars.php");
+  $dbc = mysql_connect($host, $username, $password) or die (mysql_error());
+  mysql_select_db($database_name) or die (mysql_error());
 
-	$randomLCclass = rand(1,207);
-	$query = "SELECT * FROM " . TABLE_NAME . " WHERE LC_id=$randomLCclass";
+  $randomLCclass = rand(1,207);
+  $query = "SELECT * FROM " . TABLE_NAME . " WHERE LC_id=$randomLCclass";
 
-	$result = mysql_query($query, $dbc) or die ('will not query');
+  $result = mysql_query($query, $dbc) or die ('will not query');
 
-	print 'A randomly generated call number. Hit refresh and see!'  . '</br>';
+  print 'A randomly generated call number. Hit refresh and see!'  . '</br>';
 
-	$row = mysql_fetch_row($result);
-	$randomLCrange = rand(1,$row[2]);
-	echo $row[1] . $randomLCrange . '<br/>';
+  $row = mysql_fetch_row($result);
+  $randomLCrange = rand(1,$row[2]);
 
-	// Much thanks to everyone who helped!
-	// This is still work to be done. I need to add the cutter selector 
-	// and add the rest of the call numbers to the mysql table
+  $cutter_letter=array(' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+  $random_letter_choice = rand(1,26);
+  $random_cutter_letter = rand(1,9);
+  $random_cutter_number = rand(1,9);
 
-	mysql_close($dbc);
+  echo $row[1] . $randomLCrange . '.' . $cutter_letter[$random_cutter_letter] . $random_cutter_number .  '<br/>';
+
+  // Much thanks to everyone who helped!
+
+  mysql_close($dbc);
 
 ?>
