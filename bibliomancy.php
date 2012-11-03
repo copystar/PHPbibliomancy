012891bc8882a8b169dd8dfef952a8ce16e42e42
Mita Williams <mita.williams@gmail.com>
2012-11-03 07:36:55 -0400

    updated file so it accesses entire mysql table of 207 values

--- bibliomancy.php
+++ bibliomancy.php
@@ -4,7 +4,7 @@
 	$dbc = mysql_connect($host, $username, $password) or die (mysql_error());
 	mysql_select_db($database_name) or die (mysql_error());
 
-	$randomLCclass = rand(1,7);
+	$randomLCclass = rand(1,207);
 	$query = "SELECT * FROM " . TABLE_NAME . " WHERE LC_id=$randomLCclass";
 
 	$result = mysql_query($query, $dbc) or die ('will not query');
